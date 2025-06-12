b1502fe40316efd5102b28ff19010ed5cb647d6b
Saahil <neon@saahild.com>
2025-06-12 16:50:52 -0400

    fix: url encode :(

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ if(join_requests_currently > 10) {
     console.log(event.user.id)
     // get user email 
     const info = await client.users.info({ user: event.user.id }).then(d=>d.user.profile)
-    const checkOnServersBackend = await fetch(`https://aa21-73-119-115-16.ngrok-free.app/explorpheus/magic-link?token=${env.API_KEY}&email=${info.email}&slack_id=${event.user.id}`, {
+    const checkOnServersBackend = await fetch(`https://aa21-73-119-115-16.ngrok-free.app/explorpheus/magic-link?token=${env.API_KEY}&email=${encodeURIComponent(info.email)}&slack_id=${event.user.id}`, {
         method: "POST"
     })
     if(checkOnServersBackend.status !== 200) {
